3832a32fdd8a88bd330026043677169302889fd4
vyash5075 <vyash5075@gmail.com>
2019-12-11 21:06:29 +0530

    maximum element in array

---
+++ Arrays/maxelement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class maxelement {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int max=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+                
+            }
+        }
+        System.out.println("max element in array is"+max);
+        
+        
+    }
+}
