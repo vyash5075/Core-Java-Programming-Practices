3795799b92aa82095b0a22e11027beb9352ab313
vyash5075 <vyash5075@gmail.com>
2019-12-18 21:19:58 +0530

    array to arraylist method2

---
+++ Arrays/array_arraylist2.java
@@ -0,0 +1,17 @@
+public class array_arraylist2{
+     public static void main(String args[])
+    {
+        int j=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+         List al = Arrays.asList(arr); 
+        
+        System.out.println(al); 
+}
+}
