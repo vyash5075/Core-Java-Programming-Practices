4ca875bacdc7d7bb5b5a6da1898310eb49186f7e
Yash Verma <vyash5075@gmail.com>
2021-04-12 11:10:27 +0530

    Create coinchange2.java

---
+++ recursion/coinchange2.java
@@ -0,0 +1,51 @@
+    package strings;
+    import java.util.*;
+    
+    /*
+    input=>
+    5
+    2
+    3
+    4
+    5
+    6
+    12
+
+    
+    output=>
+    2 - 4 - 6 -  .
+    3 - 4 - 5 -  .
+    
+    */
+     class newbie {
+         
+         public static void main(String args[]){
+             Scanner sc=new Scanner(System.in);
+             int noofwords=sc.nextInt();
+             int coins[]=new int[noofwords];
+             for(int i=0;i<coins.length;i++){
+                 coins[i]=sc.nextInt();
+             }
+             int tamount=sc.nextInt();
+            coinchange(0,coins,0,tamount,"");
+         }
+         
+       public static void coinchange(int i,int coins[],int currentamount,int totalamount,String asf){
+          
+           if(i==coins.length){
+               if(currentamount==totalamount){
+                   System.out.println(asf);
+               }
+               return;
+           }
+        for(int j=totalamount/coins[i];j>=1;j--){
+            String part="";
+            for(int k=0;k<j;k++){
+               part+=coins[i]+"-";
+            }
+            System.out.println(i+" "+coins[i]*j);
+             coinchange(i+1,coins,currentamount+coins[i]*j,totalamount,asf+part);
+        }
+        coinchange(i+1,coins,currentamount,totalamount,asf);
+       }
+     }
