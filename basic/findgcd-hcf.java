432fa71fafdb34e94ebc603d443e997e58d0bd2b
Yash Verma <vyash5075@gmail.com>
2021-04-20 09:20:33 +0530

    Update findgcd-hcf.java

--- basic/findgcd-hcf.java
+++ basic/findgcd-hcf.java
@@ -26,3 +26,19 @@
         }
         
      }
+-----------------------------------------------------------------------------------------------
+    //way2
+   public class gcd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        while(n1%n2!=0){
+            int rem=n1%n2;
+            n1=n2;
+            n2=rem;
+        }
+        int gcd=n2;
+        System.out.println(gcd);
+    }
+}
