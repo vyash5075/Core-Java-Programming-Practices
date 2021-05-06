d624501b8d84c15514fb33d612e009a6828eb07b
Yash Verma <vyash5075@gmail.com>
2021-05-06 10:34:22 +0530

    Update rainwatertrappingproblem.java

--- Arrays/rainwatertrappingproblem.java
+++ Arrays/rainwatertrappingproblem.java
@@ -35,3 +35,45 @@ public class ques84 {
         }
         return res;
     }
+    
+    //-------------------------------------------------------------------------------------------------------
+    //optimised approach in O(n)
+    
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author yash verma
+ */
+import java.util.*;
+public class ques84 {
+    public static void main(String args[]){
+        int[] arr = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        int n=arr.length;
+        System.out.println(findwater(arr,n));
+    }
+    
+    public static int findwater(int arr[],int n){
+        int left[]=new int[n];
+        int right[]=new int[n];
+        int water=0;
+        left[0]=arr[0];
+        for(int i=1;i<n;i++){
+            left[i]=Math.max(left[i-1], arr[i]);
+        }
+        right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=Math.max(arr[i], right[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            water+=Math.min(left[i],right[i])-arr[i];
+        }
+        return water;
+    }
+}
+
