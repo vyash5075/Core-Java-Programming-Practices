e42e7557aa11b34d1e5c883917449eeb03028ae7
Yash Verma <vyash5075@gmail.com>
2021-06-27 19:21:06 +0530

    Create minimum_in_rotatedsortedarray.java

---
+++ Searching-Sortings/minimum_in_rotatedsortedarray.java
@@ -0,0 +1,36 @@
+//input : arr[]=4,5,6,7,8,9,10,1, 
+//output: 1
+//source: https://www.youtube.com/watch?v=Kcj2NGnuSNg&list=PL-Jc9J83PIiHhXKonZxk7gbEWsmSYP5kq&index=15
+
+import java.util.*;
+public class gfg{
+    public static void main(String args[]){
+        int arr[]={4,5,6,7,8,9,10,1,2};
+        int a=findminimum(arr);
+        System.out.println(a);
+    }
+    public static int  findminimum(int arr[]){
+        
+        int lo=0;
+        int high=arr.length-1;
+        
+        while(lo<=high){
+            int mid=(lo+high)/2;
+            if(arr[mid]<arr[mid-1]){
+                return arr[mid];
+            }
+            else if(arr[mid]>arr[mid+1]){
+             return arr[mid+1];   
+            }
+            
+            
+            else if(arr[lo]<=arr[mid]){
+                lo=mid+1;
+            }
+            else if(arr[mid]<=arr[high]){
+                high=mid-1;
+            }
+        }    
+        return -1;
+    }
+}
